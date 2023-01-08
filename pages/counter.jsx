960a34f54dda38ccc6e43f1bfff418a492271c2a
yogeshnishad007 <yogiatoy27518@gmail.com>
2023-01-08 20:48:40 +0530

    new changes counter

--- pages/counter.jsx
+++ pages/counter.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useState } from 'react'
 
 const counter = () => {
+    const [count,setCount]=useState(0)
   return (
     <div>
 
-        <h1>Counter</h1>
+        <h1>Counter :{count}</h1>
+
+
+        <div>
+
+            <button onClick={()=> setCount(count+1)}>+</button>
+            <button onClick={()=> setCount(count-1)}>-</button>
+        </div>
 
            
 
